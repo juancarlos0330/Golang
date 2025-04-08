02c6aa5f721ad94004715594ba517398cdd53d6a
juancarlos0330 <juancarlos.king0330@gmail.com>
2025-04-08 23:11:24 +0900

    - Add Loop function

--- test.go
+++ test.go
@@ -231,6 +231,34 @@ func SwitchFunc() {
 	}
 }
 
+func loopsFunc() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+
+	for i := 0; i < 5; i++ {
+		if i == 3 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	for i := 0; i < 5; i++ {
+		if i == 3 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	adj := [2]string{"big", "tasty"}
+	fruits := [3]string{"apple", "orange", "banana"}
+	for i := 0; i < len(adj); i++ {
+		for j := 0; j < len(fruits); j++ {
+			fmt.Println(adj[i], fruits[j])
+		}
+	}
+}
+
 func main() {
-	SwitchFunc()
+	loopsFunc()
 }
