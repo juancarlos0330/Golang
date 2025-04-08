8ecdf03cba378e08b2b811e1e3106aef8c2049af
juancarlos0330 <juancarlos.king0330@gmail.com>
2025-04-08 16:17:39 +0900

    - Add formatting_verbs function

--- test.go
+++ test.go
@@ -64,6 +64,25 @@ func formatting_verbs() {
 	fmt.Printf("%v\n", txt)
 	fmt.Printf("%#v\n", txt)
 	fmt.Printf("%T\n", txt)
+
+	fmt.Printf("%b\n", i)
+	fmt.Printf("%d\n", i)
+	fmt.Printf("%+d\n", i)
+	fmt.Printf("%o\n", i)
+	fmt.Printf("%O\n", i)
+	fmt.Printf("%x\n", i)
+	fmt.Printf("%X\n", i)
+	fmt.Printf("%#x\n", i)
+	fmt.Printf("%4d\n", i)
+	fmt.Printf("%-4d\n", i)
+	fmt.Printf("%04d\n", i)
+
+	fmt.Printf("%s\n", txt)
+	fmt.Printf("%q\n", txt)
+	fmt.Printf("%8s\n", txt)
+	fmt.Printf("%-8s\n", txt)
+	fmt.Printf("%x\n", txt)
+	fmt.Printf("% x\n", txt)
 }
 
 func main() {
